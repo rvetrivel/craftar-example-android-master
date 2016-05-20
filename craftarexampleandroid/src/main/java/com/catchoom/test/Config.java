92c700048434f9a5d407a1466469f1098519c766
rvetrivel <vetriece12@gmail.com>
2016-05-20 15:29:05 +0530

    Initial commit

---
+++ craftarexampleandroid/src/main/java/com/catchoom/test/Config.java
@@ -0,0 +1,7 @@
+package com.catchoom.test;
+
+public class Config {
+	public final static String MY_COLLECTION_TOKEN = "b57d80cc143f4efc";
+	//public final static String MY_COLLECTION_TOKEN = "6ec453f0fbd144c4";
+
+}
